b2410876b21d29f6dd2c5173378919f7e7c1f41e
Andrew Enfield <aenfield@gmail.com>
2018-04-28 19:43:20 -0700

    Added explicit data types when loading shot data.

--- pygolfdata/data/shotlink.py
+++ pygolfdata/data/shotlink.py
@@ -1,20 +1,68 @@
 import pandas as pd
+import numpy as np
 
 DATA_PATH = '../../golf_course_project_data'
 
-shot_fields = ['Tour Code', 'Tour Description', 'Year', 'Tourn.#', 'Player #',
-              'Course #', 'Permanent Tournament #', 'Player First Name',
-              'Player Last Name', 'Round', 'Tournament Name', 'Course Name',
-              'Hole', 'Hole Score', 'Par Value', 'Yardage', 'Shot',
-              'Shot Type(S/P/D)', '# of Strokes', 'From Location(Scorer)',
-              'From Location(Enhanced)', 'To Location(Scorer)',
-              'To Location(Enhanced)', 'Distance', 'Distance to Pin',
-              'In the Hole Flag', 'Around the Green Flag', '1st Putt Flag',
-              'Distance to Hole after the Shot', 'Time', 'Lie', 'Elevation',
-              'Slope', 'X Coordinate', 'Y Coordinate', 'Z Coordinate',
-              'Distance from Center', 'Distance from Edge', 'Date', 'Left/Right',
-              'Strokes Gained/Baseline', 'Strokes Gained Category',
-              'Recovery Shot']
+shot_fields = [
+    'Tour Code', 'Tour Description', 'Year', 'Tourn.#', 'Player #',
+    'Course #', 'Permanent Tournament #', 'Player First Name',
+    'Player Last Name', 'Round', 'Tournament Name', 'Course Name',
+    'Hole', 'Hole Score', 'Par Value', 'Yardage', 'Shot',
+    'Shot Type(S/P/D)', '# of Strokes', 'From Location(Scorer)',
+    'From Location(Enhanced)', 'To Location(Scorer)',
+    'To Location(Enhanced)', 'Distance', 'Distance to Pin',
+    'In the Hole Flag', 'Around the Green Flag', '1st Putt Flag',
+    'Distance to Hole after the Shot', 'Time', 'Lie', 'Elevation',
+    'Slope', 'X Coordinate', 'Y Coordinate', 'Z Coordinate',
+    'Distance from Center', 'Distance from Edge', 'Date', 'Left/Right',
+    'Strokes Gained/Baseline', 'Strokes Gained Category',
+    'Recovery Shot' ]
+
+shot_dtypes = {
+    "Tour Code":"category",
+    "Tour Description":"category",
+    "Year": np.uint16,
+    "Tourn.#": np.uint16,
+    "Player #": np.uint16,
+    "Course #": np.uint16,
+    "Permanent Tournament #": np.uint16,
+    "Player First Name":"category",
+    "Player Last Name":"category",
+    "Round": np.uint8,
+    "Tournament Name":"category",
+    "Course Name":"category",
+    "Hole": np.uint8,
+    "Hole Score": np.float32,
+    "Par Value": np.uint8,
+    "Yardage": np.uint16,
+    "Shot": np.uint8,
+    "Shot Type(S/P/D)":"category",
+    "# of Strokes": np.uint8,
+    "From Location(Scorer)":"category",
+    "From Location(Enhanced)": object,
+    "To Location(Scorer)":"category",
+    "To Location(Enhanced)":"category",
+    "Distance": np.uint16,
+    "Distance to Pin": np.uint16,
+    "In the Hole Flag":"category",
+    "Around the Green Flag":"category",
+    "1st Putt Flag":"category",
+    "Distance to Hole after the Shot": np.uint16,
+    "Time": np.uint16,
+    "Lie":"category",
+    "Elevation":"category",
+    "Slope":"category",
+    "X Coordinate": object,
+    "Y Coordinate": object,
+    "Z Coordinate": object,
+    "Distance from Center": np.uint16,
+    "Distance from Edge": np.uint16,
+    #"Date":datetime, use parse_dates on load
+    "Left/Right":"category",
+    "Strokes Gained/Baseline": np.float32,
+    "Strokes Gained Category":"category",
+    "Recovery Shot":"category" }
+
 
 def get_shots(years, data_path=DATA_PATH):
     """
@@ -24,6 +72,11 @@ def get_shots(years, data_path=DATA_PATH):
            example [2017], or [2015, 2016], etc.
     data_path: path to the location of the Shot20xx.TXT source files.
     """
-    dfs = [pd.read_csv(f'{data_path}/Shot{year}.TXT', sep=';', encoding='ISO-8859-1',
-                       header=0, names=shot_fields) for year in years]
+
+    # need na_values as below because 'Hole Score' has double spaced empty values
+    dfs = [pd.read_csv(f'{data_path}/Shot{year}.TXT',
+                       sep=';', encoding='ISO-8859-1',
+                       header=0, names=shot_fields,
+                       dtype=shot_dtypes,
+                       na_values='  ') for year in years]
     return pd.concat(dfs, ignore_index=True)
--- pygolfdata/tests/test_shotlink_data.py
+++ pygolfdata/tests/test_shotlink_data.py
@@ -2,16 +2,31 @@ import unittest
 
 from data import shotlink
 
-class ShotsTests(unittest.TestCase):
 
+TEST_DATA_PATH = '../../golf_course_project_data/test'
+
+class ShotTests(unittest.TestCase):
+    """Tests for shot data, generally using the data subsets in the data test directory, for speed."""
     def test_get_shots_has_2017_data(self):
-        df = shotlink.get_shots([2017])
-        self.assertEqual(1214437, len(df))
+        df = shotlink.get_shots([2017], TEST_DATA_PATH)
+        self.assertEqual(999, len(df))
 
     def test_get_shots_has_two_years_worth_of_data(self):
-        df = shotlink.get_shots([2008,2009])
-        self.assertEqual(2343419, len(df))
+        df = shotlink.get_shots([2008,2009], TEST_DATA_PATH)
+        self.assertEqual(999+999, len(df))
 
-    def test_get_shots_updates_colnames(self):
-        df = shotlink.get_shots([2017])
+    def test_get_shots_updates_field_names(self):
+        df = shotlink.get_shots([2017], TEST_DATA_PATH)
         self.assertListEqual(shotlink.shot_fields, df.columns.values.tolist())
+
+    def test_get_shots_has_explicit_data_types(self):
+        df = shotlink.get_shots([2017], TEST_DATA_PATH)
+        for field_name, dtype in shotlink.shot_dtypes.items():
+            self.assertEqual(dtype, df.dtypes[field_name])
+
+
+class ShotTestsIntegration(unittest.TestCase):
+    """Tests for shot data, that take longer, including tests with actual data."""
+    def test_get_shots_has_2017_actual_data(self):
+        df = shotlink.get_shots([2017])
+        self.assertEqual(1214437, len(df))

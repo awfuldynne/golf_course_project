54731418f1acb32bc2c2a346d038920046a27f9f
Andrew Enfield <aenfield@gmail.com>
2018-05-05 00:37:04 -0700

    Added course levels get; updated shots and course levels to use dtype data for col names.

--- pygolfdata/data/shotlink.py
+++ pygolfdata/data/shotlink.py
@@ -1,24 +1,26 @@
+import collections
+
 import pandas as pd
 import numpy as np
 
 DATA_PATH = '../../golf_course_project_data'
 
-shot_fields = [
-    'TourCode', 'TourDescription', 'Year', 'TournamentNum', 'PlayerNum',
-    'CourseNum', 'PermanentTournamentNum', 'PlayerFirstName',
-    'PlayerLastName', 'Round', 'TournamentName', 'CourseName',
-    'Hole', 'HoleScore', 'ParValue', 'Yardage', 'Shot',
-    'ShotType', 'NumStrokes', 'FromLocationScorer',
-    'FromLocationEnhanced', 'ToLocationScorer',
-    'ToLocationEnhanced', 'Distance', 'DistanceToPin',
-    'InTheHoleFlag', 'AroundTheGreenFlag', 'FirstPuttFlag',
-    'DistanceToHoleAfterShot', 'Time', 'Lie', 'Elevation',
-    'Slope', 'XCoordinate', 'YCoordinate', 'ZCoordinate',
-    'DistanceFromCenter', 'DistanceFromEdge', 'Date', 'LeftRight',
-    'StrokesGainedBaseline', 'StrokesGainedCategory',
-    'RecoveryShot' ]
+# shot_fields = [
+#     'TourCode', 'TourDescription', 'Year', 'TournamentNum', 'PlayerNum',
+#     'CourseNum', 'PermanentTournamentNum', 'PlayerFirstName',
+#     'PlayerLastName', 'Round', 'TournamentName', 'CourseName',
+#     'Hole', 'HoleScore', 'ParValue', 'Yardage', 'Shot',
+#     'ShotType', 'NumStrokes', 'FromLocationScorer',
+#     'FromLocationEnhanced', 'ToLocationScorer',
+#     'ToLocationEnhanced', 'Distance', 'DistanceToPin',
+#     'InTheHoleFlag', 'AroundTheGreenFlag', 'FirstPuttFlag',
+#     'DistanceToHoleAfterShot', 'Time', 'Lie', 'Elevation',
+#     'Slope', 'XCoordinate', 'YCoordinate', 'ZCoordinate',
+#     'DistanceFromCenter', 'DistanceFromEdge', 'Date', 'LeftRight',
+#     'StrokesGainedBaseline', 'StrokesGainedCategory',
+#     'RecoveryShot' ]
 
-shot_dtypes = {
+shot_dtypes = collections.OrderedDict({
     "TourCode": "category",
     "TourDescription": "category",
     "Year": np.uint16,
@@ -57,12 +59,66 @@ shot_dtypes = {
     "ZCoordinate": object,
     "DistanceFromCenter": np.uint16,
     "DistanceFromEdge": np.uint16,
-    #"Date":datetime, use parse_dates on load
+    "Date": object, # use parse_dates while preparing; takes too long to do on read_csv
     "LeftRight": "category",
     "StrokesGainedBaseline": np.float32,
     "StrokesGainedCategory": "category",
-    "RecoveryShot": "category" }
+    "RecoveryShot": "category" })
 
+# courselevel_fields = [
+#     'Year', 'CourseNum', 'CourseName', 'Round', 'Hole', 'FwyWidth250',
+#     'Actual250Distance', 'FwyWidth275', 'Actual275Distance',
+#     'FwyWidth300', 'Actual300Distance', 'FwyWidth325',
+#     'Actual325 Distance', 'FwyWidth350', 'Actual350Distance',
+#     'FwyFirmness', 'GrnFirmness', 'GrnHeight', 'RoughHeight',
+#     'FwyHeight', 'Stimp', 'AMWindSpd', 'AMWindDir', 'PMWindSpd',
+#     'PMWind Dir', 'ScorecardYdg', 'ActualYdg', 'Par', 'GreenGrass',
+#     'FwyGrass', 'TeeGrass', 'BunkerGrass', 'RoughGrass', 'ExtraColumn' ]
+
+# size of the source dataframe is much less than shots, but I'll set dtypes explicitly
+# since we'll be pulling this data into a joined dataframe
+courselevel_dtypes = collections.OrderedDict({
+    "Year": np.uint16,
+    "CourseNum": np.uint16,
+    "CourseName": 'category',
+    "Round": np.uint8,
+    "Hole": np.uint8,
+    "FwyWidth250": np.uint16,
+    "Actual250Distance": np.uint16,
+    "FwyWidth275": np.uint16,
+    "Actual275Distance": np.uint16,
+    "FwyWidth300": np.uint16,
+    "Actual300Distance": np.uint16,
+    "FwyWidth325": np.uint16,
+    "Actual325 Distance": np.uint16,
+    "FwyWidth350": np.uint16,
+    "Actual350Distance": np.uint16,
+    "FwyFirmness": 'category',
+    "GrnFirmness": 'category',
+    "GrnHeight": np.float32,
+    "RoughHeight": np.float32,
+    "FwyHeight": np.float32,
+    "Stimp": 'category',
+    "AMWindSpd": 'category',
+    "AMWindDir": 'category',
+    "PMWindSpd": 'category',
+    "PMWind Dir": 'category',
+    "ScorecardYdg": np.uint16,
+    "ActualYdg": np.float32, # has some NaNs
+    "Par": np.uint8,
+    "GreenGrass": 'category',
+    "FwyGrass": 'category',
+    "TeeGrass": 'category',
+    "BunkerGrass": 'category',
+    "RoughGrass": 'category',
+    "ExtraColumn": object})
+
+
+def get_years(type_prefix, data_path, years, **kwargs):
+    """Module internal function to retrieve specified data; called by get_shots, etc."""
+    dfs = [pd.read_csv(f'{data_path}/{type_prefix}{year}.TXT',
+                       sep=';', encoding='ISO-8859-1', **kwargs) for year in years]
+    return pd.concat(dfs, ignore_index=True)
 
 def get_shots(years, data_path):
     """
@@ -74,14 +130,11 @@ def get_shots(years, data_path):
 
     :return: a DataFrame containing a row per shot, for the specified year(s).
     """
-
     # need na_values as below because 'Hole Score' has double spaced empty values
-    dfs = [pd.read_csv(f'{data_path}/Shot{year}.TXT',
-                       sep=';', encoding='ISO-8859-1',
-                       header=0, names=shot_fields,
-                       dtype=shot_dtypes,
-                       na_values='  ') for year in years]
-    return pd.concat(dfs, ignore_index=True)
+    return get_years('Shot', data_path, years,
+                     header=0, names=shot_dtypes.keys(),
+                     dtype=shot_dtypes,
+                     na_values='  ')
 
 def prepare_shots(df):
     """
@@ -94,3 +147,21 @@ def prepare_shots(df):
     df['Date'] = pd.to_datetime(df['Date'])
 
     return df
+
+
+def get_courselevels(years, data_path):
+    """
+    Loads course level data.
+
+    Course level data has a bunch of data that could correlate with performance (fairway width, firmness,
+    grass height, stimpmeter, as well as recorded wind data that we might be able to use in comparison
+    to the wind data we get separately.
+
+    :param years: a sequence of integer year values for which data is desired; for
+                  example [2017], or [2015, 2016], etc.
+    :param data_path: path to the location of the source files.
+
+    :return: a DataFrame containing a row per course level, for the specified year(s).
+    """
+    return get_years('CourseLevel', data_path, years,
+                     header=0, names=courselevel_dtypes.keys(), dtype=courselevel_dtypes)

a61ba56eeb61f36dc0802dd9bf6fd3e6fad5ec36
Sean Miller <millsea0@uw.edu>
2018-05-10 20:27:54 -0700

    Adding working weather scraping script to source control

--- pygolfdata/weather/core.py
+++ pygolfdata/weather/core.py
@@ -1,14 +1,18 @@
 """ pygolfdata - weather module
 
 Provides an interface to maintain and update a pandas DataFrame containing hourly weather data.
+
+Classes:
+    WeatherDateApi
+        Class that leverages the DarkSky API to extract hourly weather data per day. Manages a pandas DataFrame and
+        also writes the DataFrame out to CSV to preserve already gathered weather data.
 """
+from datetime import datetime
 import time
-import json
 import os
 import pandas as pd
 
 from darksky import forecast
-from WunderWeather import weather
 
 
 class WeatherDateApi:
@@ -17,14 +21,14 @@ class WeatherDateApi:
     weather data.
 
     DataFrame Columns:
-    ['Date', 'Hour', 'Summary', 'DegreesFahrenheit', 'Humidity', 'Visibility', 'WindBearing', 'WindGust', 'WindSpeed']
+    ['Date', 'Hour', 'Latitude', 'Longitude', 'Summary', 'DegreesFahrenheit', 'Humidity', 'Visibility', 'WindBearing',
+    'WindGust', 'WindSpeed']
     """
-    COLUMNS = ['Date', 'Hour', 'Summary', 'DegreesFahrenheit', 'Humidity', 'Visibility', 'WindBearing', 'WindGust',
-               'WindSpeed']
+    COLUMNS = ['Date', 'Hour', 'Latitude', 'Longitude', 'Summary', 'DegreesFahrenheit', 'Humidity', 'Visibility',
+               'WindBearing', 'WindGust', 'WindSpeed', "PrecipitationIntensity", "PrecipitationType"]
 
     def __init__ (self, api_key, weather_history_file_path):
         self.__api_key = api_key
-        self.__extractor = weather.Extract(api_key)
         self.__file_path = weather_history_file_path
 
         if os.path.isfile(weather_history_file_path):
@@ -38,18 +42,16 @@ class WeatherDateApi:
     def set_api_key(self, new_api_key):
         """Overwrites current API key and sets extractor to """
         self.__api_key = new_api_key
-        self.__extractor = weather.Extract(self.__api_key)
 
     def get_weather_dataframe(self):
         return self.__df
 
-    def append_weather_data(self, latitude, longitude, start_date, end_date, write_new_csv=False):
+    def append_weather_data(self, latitude, longitude, app_date, write_new_csv=False):
         """Function to append new weather data to the weather DataFrame.
 
         :param latitude: Floating point value representing latitude
         :param longitude: Floating point value representing longitude
-        :param start_date: Starting date of date range
-        :param end_date: Ending date of date range
+        :param date: Datetime to retrieve weather data
         :param write_new_csv: Flag to update csv output after data is appended
         :return: Nothing
         """
@@ -60,25 +62,16 @@ class WeatherDateApi:
         if not (-180 <= longitude <= 180):
             raise ValueError('Longitude is outside the expected range')
 
-        start_epoch_time = time.mktime(start_date)
-        end_epoch_time = time.mktime(end_date)
+        epoch_time = time.mktime(app_date.timetuple())
 
-        # TODO: Add check to not retrieve data that is already a part of the DataFrame
-        loop_epoch_time = start_epoch_time
-        while loop_epoch_time <= end_epoch_time:
-            # Get response from API
-            response = self.__get_weather_json_darksky(latitude, longitude, loop_epoch_time)
+        # Retrieve data from DarkSky API
+        response = self.__get_weather_json_darksky(latitude, longitude, epoch_time)
 
-            # Iterate through response and clean
-            for hourly_snapshot in response['hourly']['data']:
-                print(hourly_snapshot)
-
-            # Append to DataFrame
-            self.__df = self.__df.append(self.__get_weather_series_from_response, ignore_index=True)
-
-            # This is an unsafe assumption but good enough for now
-            # https://stackoverflow.com/questions/7552104/is-a-day-always-86-400-epoch-seconds-long
-            loop_epoch_time += 86400
+        # Iterate through response and append to DataFrame
+        for hourly_snapshot in response['hourly']['data']:
+            self.__df =\
+                self.__df.append(self.__get_weather_series_from_response(latitude, longitude, hourly_snapshot),
+                                 ignore_index=True)
 
         # If write_new_csv is True, write out the updated DF
         if write_new_csv:
@@ -93,17 +86,27 @@ class WeatherDateApi:
 
     def __get_weather_json_darksky(self, latitude, longitude, epoch_time):
         """ """
-        response = forecast(self.__api_key, latitude, longitude, epoch_time)
-        json_response = None
-        try:
-            json_response = json.loads(response)
-        except:
-            print('Bad things have happened')
-        return json_response
+        response = forecast(self.__api_key, latitude, longitude, int(epoch_time))
+        return response
 
-    def __get_weather_series_from_response(self, response):
+    def __get_weather_series_from_response(self, latitude, longitude, response):
         """ """
-        row_data = ['date', 0, response['summary'], response['temperature'], response['humidity'],
-                    response['visibility'], response['windBearing'], response['windGust'], response['windSpeed']]
+        print(response)
+        response_keys = response.keys()
+
+        date_time = datetime.fromtimestamp(response['time'])
+        row_data = [date_time.strftime('%Y-%m-%d'),
+                    date_time.hour,
+                    latitude,
+                    longitude,
+                    response['summary'] if 'summary' in response_keys else None,
+                    response['temperature'] if 'temperature' in response_keys else None,
+                    response['humidity'] if 'humidity' in response_keys else None,
+                    response['visibility'] if 'visibility' in response_keys else None,
+                    response['windBearing'] if 'windBearing' in response_keys else None,
+                    response['windGust'] if 'windGust' in response_keys else None,
+                    response['windSpeed'] if 'windSpeed' in response_keys else None,
+                    response['precipIntensity'] if 'precipIntensity' in response_keys else None,
+                    response['precipType'] if 'precipType' in response_keys else None]
         row = pd.Series(row_data, index=self.COLUMNS)
         return row

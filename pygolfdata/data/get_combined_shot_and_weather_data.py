303065d928673345d7d2fbcfedb63f01bafe2569
Andrew Enfield <aenfield@gmail.com>
2018-05-15 20:28:12 -0700

    Resolves #9 by automating everything but the commit/push.
    Now the script takes starting and ending years, saves the csv file with the
    appropriate filename, zips the csv file, and copies it to the data repo location.

--- pygolfdata/data/get_combined_shot_and_weather_data.py
+++ pygolfdata/data/get_combined_shot_and_weather_data.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import zipfile
+import shutil  # for copy file function copy2
 
 import pandas as pd
 
@@ -7,14 +9,17 @@ import shotlink
 
 def main():
     data_path = '../../../golf_course_project_data'
-    # years = [2017]
-    years = range(2012, 2017)
 
-    print(f'Started at {datetime.now()}.')
+    START_YEAR = 2012
+    END_YEAR = 2016
+    years = range(START_YEAR, END_YEAR + 1)
+
+    print(f'Started at {datetime.now()}, for years {START_YEAR} to {END_YEAR}.')
+
     weather = pd.read_csv(f'{data_path}/pga_tour_weather_data.csv')
     print(f'Loaded weather data with {len(weather)} rows and {len(weather.columns)} columns.')
 
-    # interim fix for a few rows w/ hour values of 24
+    # the data can have hour values of 24 (at least at one time), when normal changes to daylight savings
     weather = weather[weather['Hour'] != 24]
 
     weather['WeatherDateAndHour'] = pd.to_datetime(weather['Date'] + ' ' + weather['Hour'].astype(str).str.zfill(2))
@@ -31,8 +36,18 @@ def main():
                              suffixes=['_shots', '_weather'], direction='nearest')
     combined['TimeDifference'] = combined['ShotDateAndTime'] - combined['WeatherDateAndHour']
 
-    print(f'Writing {len(combined)} rows with {len(combined.columns)} columns...')
-    combined.to_csv('combined_shots_and_weather.csv', index=False)
+    csv_filename = f'combined_shots_and_weather_{START_YEAR}_{END_YEAR}.csv'
+    print(f'Writing {len(combined)} rows with {len(combined.columns)} columns, to {csv_filename}...')
+    combined.to_csv(csv_filename, index=False)
+
+    print('Zipping...')
+    zip_filename = f'combined{START_YEAR}to{END_YEAR}.zip'
+    with zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED) as zip_ref:
+        zip_ref.write(csv_filename)
+
+    print('Copying...')
+    shutil.copy2(zip_filename, data_path) # copy2 allows dest to be a dir (poorly named function)
+
     print(f'Done at {datetime.now()}.')
 
 if __name__ == '__main__':

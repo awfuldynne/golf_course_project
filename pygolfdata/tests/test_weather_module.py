2b2b51395cad177d1ba79b2cf02232cc3425752e
Sean Miller <millsea0@uw.edu>
2018-05-19 16:39:28 -0700

    Fixing pylint errors in test_weather_module

--- pygolfdata/tests/test_weather_module.py
+++ pygolfdata/tests/test_weather_module.py
@@ -1,10 +1,11 @@
 """ Unit tests for pygolfdata weather module
 
-This module validates the functionality of the WeatherDateApi class exposed under core in the weather library.
+This module validates the functionality of the WeatherDateApi class exposed
+under core in the weather library.
 
 Classes:
     WeatherDateApiTest(unittest.TestCase)
-        Framework for testing the core functionality of the WeatherDateApi class.
+        Framework for testing the functionality of the WeatherDateApi class.
 """
 
 
@@ -19,20 +20,28 @@ from weather import core
 class WeatherDateApiTest(unittest.TestCase):
     """ Class of unit tests for the WeatherDateApi's core functionality
     """
-    #valid_file_path = "weather/test_data/weather_date_api_test.csv"
-    existing_valid_file_path = "E:\Data\data_515\etest.csv"
-    non_existing_valid_file_path = "E:\Data\data_515\esdfs.csv"
-    invalid_file_path = "weather/test_data/BAD_DIRECTORY/weather_date_api_test.csv"
+    existing_valid_file_path = "weather/test_data/weather_date_api_test.csv"
+    non_existing_valid_file_path = \
+        "weather/test_data/weather_date_api_test2.csv"
+    invalid_file_path = \
+        "weather/test_data/BAD_DIRECTORY/weather_date_api_test.csv"
 
     def setUp(self):
-        self.wda = core.WeatherDateApi("TEST", self.existing_valid_file_path)
-        self.wda2 = core.WeatherDateApi("TEST", self.non_existing_valid_file_path)
+        self.wda = \
+            core.WeatherDateApi("TEST", self.existing_valid_file_path)
+        self.wda2 = \
+            core.WeatherDateApi("TEST", self.non_existing_valid_file_path)
         self.df = self.wda.get_weather_dataframe()
 
     def test_invalid_file_path(self):
-        """ Tests that the __init__ method throws a ValueError when given a bad file path
+        """ Tests that the __init__ method throws a ValueError when given a bad
+        file path
         """
-        self.assertRaises(ValueError, core.WeatherDateApi, "", self.invalid_file_path)
+        self.assertRaises(
+            ValueError,
+            core.WeatherDateApi,
+            "",
+            self.invalid_file_path)
 
     def test_get_api_key(self):
         """ Tests the API key get method
@@ -42,25 +51,34 @@ class WeatherDateApiTest(unittest.TestCase):
     def test_set_api_key(self):
         """ Tests the API key set method
         """
-        random_key = ''.join(random.choice(string.ascii_uppercase) for _ in range(10))
+        random_key = \
+            ''.join(random.choice(string.ascii_uppercase) for _ in range(10))
         self.wda.set_api_key(random_key)
         self.assertEqual(random_key, self.wda.get_api_key())
 
     def test_get_file_path(self):
         """ Tests the file path get method
         """
-        self.assertEqual(self.existing_valid_file_path, self.wda.get_output_file_path())
+        self.assertEqual(
+            self.existing_valid_file_path,
+            self.wda.get_output_file_path())
 
     def test_set_file_path(self):
         """ Tests the file path set method
         """
         self.wda.set_output_file_path(self.non_existing_valid_file_path)
-        self.assertEqual(self.non_existing_valid_file_path, self.wda.get_output_file_path())
+        self.assertEqual(
+            self.non_existing_valid_file_path,
+            self.wda.get_output_file_path())
 
     def test_set_invalid_file_path(self):
-        """ Tests that the file path set method throws a ValueError when given a bad file path
+        """ Tests that the file path set method throws a ValueError when given
+        a bad file path
         """
-        self.assertRaises(ValueError, self.wda.set_output_file_path, self.invalid_file_path)
+        self.assertRaises(
+            ValueError,
+            self.wda.set_output_file_path,
+            self.invalid_file_path)
 
     def test_column_names(self):
         """ Tests that all expected columns exist in the DataFrame

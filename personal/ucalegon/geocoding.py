16d1d9a211b2ee16d0be39af7cd51ba8544aa274
Andrew Smith <ucalegon@uw.edu>
2018-05-20 15:20:32 -0700

    recommitting without api_key

---
+++ personal/ucalegon/geocoding.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import requests
+
+url = "https://maps.googleapis.com/maps/api/geocode/json"
+
+# This is my API key attached to my own credit card
+api_key = 'insert_your_own_api_key_here'
+
+PATH = './golf_course_project_data/'
+courses = pd.read_csv(PATH + 'courses.txt')
+
+latlong = []
+results = None
+
+for name in courses['Course Name']:
+    params = {'key': api_key, 'address': name}
+    print(name)
+    r = requests.get(url, params=params)
+    results = r.json()['results']
+    print(results)
+    location = results[0]['geometry']['location']
+    lat = location['lat']
+    long = location['lng']
+    formatted_address = results[0]['formatted_address']
+    latlong.append([name, lat, long, formatted_address])
+
+latlong_df = pd.DataFrame(data=latlong, columns=['name','lat','long','address'])
+
+latlong_df.to_csv(PATH + 'courses_geocoded.txt')
+
+

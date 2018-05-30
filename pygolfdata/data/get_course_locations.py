cfd7eb57e22a2b3ed73c55d2acfc8e4990979c31
ucalegon1979 <king.hrothgar@gmail.com>
2018-05-29 20:27:20 -0700

    Altered for more reproducibility

--- pygolfdata/data/get_course_locations.py
+++ pygolfdata/data/get_course_locations.py
@@ -3,15 +3,21 @@ Obtains the latitude and longitude for each course, outputting the result to a f
 courses_geocoded.txt.
 """
 import pandas as pd
+import os
 import requests
+
+from pygolfdata.data import shotlink
+
+fname = '../../../golf_course_project_data/combined2016to2016.zip'
+
+# Get a list of unique course names
+df = shotlink.get_combined_data_from_file(fname)
+courses = df['Course Name'].unique()
 
 url = "https://maps.googleapis.com/maps/api/geocode/json"
 
 # This is my API key attached to my own credit card
 api_key = 'insert_your_own_api_key_here'
-
-PATH = './golf_course_project_data/'
-courses = pd.read_csv(PATH + 'courses.txt')
 
 latlong = []
 results = None

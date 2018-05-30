6a67535068a50aa433c44904019406f9e7e3ee71
Andrew Enfield <aenfield@gmail.com>
2018-05-29 22:19:27 -0700

    Updated Andrew Smith's code to use shot data that doesn't require previous processing.
    The last change updated the geocoding code to use a file that was itself generated
    using geocoded locations. This could be fine because that file does exist, now that
    we've already generated it, but it does mean that a user that just has access to the
    stock ShotLink data couldn't run it. Now the code uses just stock ShotLink data.
    I also made a few other tweaks to make it run, including updating the output filename
    to not use an undefined constant.

--- pygolfdata/data/get_course_locations.py
+++ pygolfdata/data/get_course_locations.py
@@ -3,39 +3,39 @@ Obtains the latitude and longitude for each course, outputting the result to a f
 courses_geocoded.txt.
 """
 import pandas as pd
-import os
 import requests
 
-from pygolfdata.data import shotlink
+import shotlink
 
-fname = '../../../golf_course_project_data/combined2016to2016.zip'
+def main():
+    # Get a list of unique course names
+    df = shotlink.get_shots(range(2012, 2018), 'data') # just get 2012-2017, for now at least
+    courses = df['CourseName'].unique()
 
-# Get a list of unique course names
-df = shotlink.get_combined_data_from_file(fname)
-courses = df['Course Name'].unique()
+    url = "https://maps.googleapis.com/maps/api/geocode/json"
 
-url = "https://maps.googleapis.com/maps/api/geocode/json"
+    # This is my API key attached to my own credit card
+    api_key = 'insert_your_own_api_key_here'
 
-# This is my API key attached to my own credit card
-api_key = 'insert_your_own_api_key_here'
+    latlong = []
+    results = None
 
-latlong = []
-results = None
+    for name in courses:
+        params = {'key': api_key, 'address': name}
+        print(name)
+        r = requests.get(url, params=params)
+        results = r.json()['results']
+        print(results)
+        location = results[0]['geometry']['location']
+        lat = location['lat']
+        long = location['lng']
+        formatted_address = results[0]['formatted_address']
+        latlong.append([name, lat, long, formatted_address])
 
-for name in courses['Course Name']:
-    params = {'key': api_key, 'address': name}
-    print(name)
-    r = requests.get(url, params=params)
-    results = r.json()['results']
-    print(results)
-    location = results[0]['geometry']['location']
-    lat = location['lat']
-    long = location['lng']
-    formatted_address = results[0]['formatted_address']
-    latlong.append([name, lat, long, formatted_address])
+    latlong_df = pd.DataFrame(data=latlong, columns=['name', 'lat', 'long', 'address'])
 
-latlong_df = pd.DataFrame(data=latlong, columns=['name','lat','long','address'])
-
-latlong_df.to_csv(PATH + 'courses_geocoded.txt')
+    latlong_df.to_csv('courses_geocoded.txt')
 
 
+if __name__ == '__main__':
+    main()

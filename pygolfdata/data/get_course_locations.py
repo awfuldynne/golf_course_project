3f9a05f9790ff661f350e7e1bfda2fec99988c9e
ucalegon1979 <king.hrothgar@gmail.com>
2018-05-30 00:40:18 -0700

    corrected pylint issues

--- pygolfdata/data/get_course_locations.py
+++ pygolfdata/data/get_course_locations.py
@@ -31,23 +31,29 @@ def get_unique_values_from_zip(zip_file, csv_file, field_num):
 
 
 def main():
+    """ Main function to read a compressed CSV file within a zip archive and extract
+            unique values from a particular field, line by line, without unzipping.
+            It will include the header in the result, if it exists.
+
+        Returns:
+            will write out unique course names with latitude and longitude to csv file
+    """
     url = "https://maps.googleapis.com/maps/api/geocode/json"
 
     # This is my API key attached to my own credit card
     api_key = 'insert_your_own_api_key_here'
 
-    # The file containing the golf course information
-    archive = '../../../golf_course_project_data/combined2012to2016.zip'
-    file_name = 'combined_shots_and_weather_2012_2016.csv'
-    field_name = 'CourseName_shots'
-
     """Get a list of unique course names, then send a separate request for each course."""
-    courses = get_unique_values_from_zip(archive, file_name, 11)
-    # Then remove the field name from the list of unique courses
-    courses.remove(field_name)
+    courses = get_unique_values_from_zip(
+        zip_file='../../../golf_course_project_data/combined2012to2016.zip',
+        csv_file='combined_shots_and_weather_2012_2016.csv',
+        field_num=11)
 
+    # Then remove the field name from the list of unique courses
+    courses.remove('CourseName_shots')
+
+    # 
     latlong = []
-    results = None
 
     for name in courses:
         params = {'key': api_key, 'address': name}

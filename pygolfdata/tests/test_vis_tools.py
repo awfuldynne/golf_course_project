8580c44609b01f7bf15344a2c99825a55107bd9f
Eric Pripstein <ericpripstein@gmail.com>
2018-06-05 19:14:17 -0700

    Updated test_vis_tools.py to be pylint compliant

--- pygolfdata/tests/test_vis_tools.py
+++ pygolfdata/tests/test_vis_tools.py
@@ -1,14 +1,14 @@
 """
 Unit tests functions in the vis_tools.py module
+
+Classes:
+    VisTests: Unit tests visualization functions in vis_tools.py
 """
 import unittest
 
-import pandas as pd
-import numpy as np
-
-# pylint isn't seeing the shotlink module here, while these tests run fine, so it's a false pos
+# pylint isn't seeing the shotlink module or models module here, while these tests run fine, so it's a false pos
 from data import shotlink # pylint: disable=no-name-in-module
-from models import vis_tools
+from models import vis_tools # pylint: disable=no-name-in-module
 
 
 # DATA_PATH points to the full data, TEST_DATA_PATH to files w/ the same
@@ -18,19 +18,26 @@ DATA_PATH = 'data'
 TEST_DATA_PATH = 'data/test'
 
 class VisTests(unittest.TestCase):
-	'''
-	'''
-	def setUp(self):
-		self.df = shotlink.get_combined_data_from_file(
+    '''Tests the two plotting functions in vis_tools.py
+
+    Methods:
+        setUp
+        test_scatter
+        test_heatmap
+    '''
+    def setUp(self):
+        self.df = shotlink.get_combined_data_from_file(
             f'{TEST_DATA_PATH}/combined_shots_and_weather_2016_2016.csv')
 
-	def test_scatter(self):
-		vis_tools.plot_strokes_gained_scatter(df,'PrecipitationIntensity')
+    def test_scatter(self):
+        '''Tests the scatter plotting function
+        '''
+        vis_tools.plot_strokes_gained_scatter(self.df, 'PrecipitationIntensity')
 
-	def test_heatmap(self):
-		vis_tools.plot_heatmaps(df)
+    def test_heatmap(self):
+        '''Tests the heatmap function
+        '''
+        vis_tools.plot_heatmaps(self.df)
 
 if __name__ == '__main__':
     unittest.main()
-
-
